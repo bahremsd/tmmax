280d2d8f31fbe134828b5da406279e4e28812a24
bahremsd <bdanis23@ku.edu.tr>
2024-09-12 12:24:50 +0300

    initialize fresnel_p

--- tmmax/fresnel.py
+++ tmmax/fresnel.py
@@ -41,3 +41,18 @@ def _fresnel_s(_first_layer_n: Union[float, jnp.ndarray],
     
     # Return the reflection and transmission coefficients as a JAX array
     return jnp.array([r_s, t_s])
+
+def _fresnel_p(_first_layer_n: Union[float, jnp.ndarray], 
+               _second_layer_n: Union[float, jnp.ndarray],
+               _first_layer_theta: Union[float, jnp.ndarray], 
+               _second_layer_theta: Union[float, jnp.ndarray]) -> Tuple[jnp.ndarray, jnp.ndarray]:
+
+
+    r_p = ((_second_layer_n * jnp.cos(_first_layer_theta) - _first_layer_n * jnp.cos(_second_layer_theta)) /
+           (_second_layer_n * jnp.cos(_first_layer_theta) + _first_layer_n * jnp.cos(_second_layer_theta)))
+
+
+    t_p = (2 * _first_layer_n * jnp.cos(_first_layer_theta) /
+           (_second_layer_n * jnp.cos(_first_layer_theta) + _first_layer_n * jnp.cos(_second_layer_theta)))
+
+    return jnp.array([r_p, t_p])

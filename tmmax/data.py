c0be8a54e31757691b4c2129c32f99966b4b71a2
bahremsd <bdanis23@ku.edu.tr>
2024-09-12 11:57:19 +0300

    import load_nk_data in data

--- tmmax/data.py
+++ tmmax/data.py
@@ -8,3 +8,21 @@ import os # Importing os to handle file paths
 import pandas as pd # Importing pandas to handle CSV data
 from typing import Union, Callable # Type hints for function signatures
 import warnings # Importing the warnings module to handle warnings in the code
+
+@lru_cache(maxsize=32)
+def load_nk_data(material_name: str = '') -> Union[jnp.ndarray, None]:
+
+
+    # Construct the file path and check if the file exists
+    file_path = os.getcwd() + "/" + os.path.join('nk_data', f'{material_name}.csv')  # Create the full path to the file
+
+    # Load the data from the CSV file
+    try:
+        # Load the CSV data as a JAX array (important for using JAX's functionality, like automatic differentiation)
+        data = jnp.asarray(pd.read_csv(file_path, skiprows=1, header=None).values)  
+    except Exception as e:
+        # If an error occurs during file reading or conversion, raise an IOError
+        raise IOError(f"An error occurred while loading data for '{material_name}': {e}")
+    
+
+    return data  # Return the loaded data as a JAX array

b770fa27519d84474c410d9f0b07adb91d8c27fd
bahremsd <bdanis23@ku.edu.tr>
2024-09-13 15:24:06 +0300

    initialize compute kz for single wl and angle

--- tmmax/tmm.py
+++ tmmax/tmm.py
@@ -10,3 +10,12 @@ from data import interpolate_nk
 from reflect_transmit import _compute_rt_one_wl, _create_phases_ts_rs, _calculate_transmittace_from_coeff
 
 
+def _compute_kz_single_wl_angle_point(
+    nk_list: jnp.ndarray,          
+    layer_angles: Union[int, jnp.ndarray], 
+    wavelength: Union[int, jnp.ndarray]     
+) -> jnp.ndarray:                   
+
+
+
+    return 2 * jnp.pi * nk_list * jnp.cos(layer_angles) / wavelength  

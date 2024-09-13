517eee06a7f671d86d1a252709a5564aa10cd8a9
bahremsd <bdanis23@ku.edu.tr>
2024-09-13 15:48:59 +0300

    add func info for get nk values

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -10,5 +10,16 @@ from typing import List, Callable
 speed_of_light = 299792458 # m/s
 
 def get_nk_values(wl: float, nk_functions: List[Callable[[float], complex]], material_list: List[int]) -> np.ndarray:
+    """
+    This function retrieves the refractive index and extinction coefficient values 
+    (represented as complex numbers) for a given wavelength `wl` from a list of materials.
+    Each material has a corresponding function in the `nk_functions` list that, 
+    when provided with the wavelength, returns the refractive index (n) and extinction 
+    coefficient (k) as a complex number (n + j*k). The materials are indexed 
+    by `material_list` to access their corresponding functions.
 
+
+
+    """
+    
     return np.array([nk_functions[mat_idx](wl) for mat_idx in material_list])  # Convert the resulting list to a NumPy array.

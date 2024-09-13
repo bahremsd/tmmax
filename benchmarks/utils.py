049655dcaa440c769930490c4afb214a8be4d290
bahremsd <bdanis23@ku.edu.tr>
2024-09-13 15:47:47 +0300

    initialize get nk values func

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -8,3 +8,7 @@ from typing import List, Callable
 
 
 speed_of_light = 299792458 # m/s
+
+def get_nk_values(wl: float, nk_functions: List[Callable[[float], complex]], material_list: List[int]) -> np.ndarray:
+
+    return np.array([nk_functions[mat_idx](wl) for mat_idx in material_list])  # Convert the resulting list to a NumPy array.

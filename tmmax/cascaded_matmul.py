e181ea09232e5999d4e738a3794a24e49cb3dd6a
bahremsd <bdanis23@ku.edu.tr>
2024-09-13 14:13:04 +0300

    import libs and enable x64

---
+++ tmmax/cascaded_matmul.py
@@ -0,0 +1,4 @@
+import jax
+jax.config.update('jax_enable_x64', True) # Ensure high precision (64-bit) is enabled in JAX
+import jax.numpy as jnp # Import JAX's version of NumPy for differentiable computations
+
